4ce8acfd7ac137b6485582556a78b242c8195ec7
uuberr <60771293+uuberr@users.noreply.github.com>
2020-02-28 10:14:05 +0200

    Add files via upload

---
+++ dsdadasd/js/main.js
@@ -0,0 +1,37 @@
+var yhteensa = 5;
+var pisteet = 0;
+var huom = 0;
+
+function lahetaVastaukset()
+{
+var k1 = document.forms["kyselyLomake"]["k1"].value;
+var k2 = document.forms["kyselyLomake"]["k2"].value;
+var k3 = document.forms["kyselyLomake"]["k3"].value;
+var k4 = document.forms["kyselyLomake"]["k4"].value;
+var k5 = document.forms["kyselyLomake"]["k5"].value;
+
+for(i = 1; i <= yhteensa; i++)
+{
+    if(eval('k'+i) == null || eval('k'+i) == '')
+    {
+        alert('Et vastannut kysymykseen numero: ' + i);
+        huom = 1;
+    }
+    if(huom == 1) return false;
+}
+
+var vastaukset = ["b", "a", "d", "b", "d"];
+
+for(i = 1; i <= yhteensa; i++)	
+{
+    if(eval('k'+i) == vastaukset[i-1])
+    {
+        pisteet++;
+    }
+}
+
+var tulokset = document.getElementById('tulokset');
+alert('Sait ' + pisteet + 'pistettä kun maksimi pistemäärä oli ' + yhteensa);
+tulokset.innerHTML = '<h3>Sait <span>' + pisteet + '</span> pistettä, kun maksimi pistemäärä oli <span>' + yhteensa + '</span></h3>';
+return false;
+}
