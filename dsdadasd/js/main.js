1eec05c58544d8bc46297e88eee555f303e501d8
uuberr <60771293+uuberr@users.noreply.github.com>
2020-02-28 10:23:02 +0200

    Update main.js

--- dsdadasd/js/main.js
+++ dsdadasd/js/main.js
@@ -33,5 +33,6 @@ for(i = 1; i <= yhteensa; i++)
 var tulokset = document.getElementById('tulokset');
 alert('Sait ' + pisteet + 'pistettä kun maksimi pistemäärä oli ' + yhteensa);
 tulokset.innerHTML = '<h3>Sait <span>' + pisteet + '</span> pistettä, kun maksimi pistemäärä oli <span>' + yhteensa + '</span></h3>';
+pisteet = 0;
 return false;
 }
